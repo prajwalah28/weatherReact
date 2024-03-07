da038d4a3c2aa1871a8110330971413521f3f95e
prajwaldeveloper <prajwalhelunde@gmail.com>
2024-03-07 15:46:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container, Typography, Grid, Card, CardContent } from '@material-ui/core';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
-  const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
 
-  const fetchNotes = async () => {
-    try {
-      const response = await axios.get('https://api.gyanibooks.com/library/get_dummy_notes');
-      setNotes(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  // git remote set-url origin https://github.com/prajwalah28/materialuii.git
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d"
+  const [inputCity, setInputCity] = useState("")
+  const [data, setData] = useState({})
+
+
+  const getWetherDetails = (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data) 
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value)
+    setInputCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWetherDetails(inputCity)
+  }
+
 
   return (
-    <Container maxWidth="md" style={{ marginTop: '2rem' }}>
-      <Typography variant="h4" component="h1" align="center" gutterBottom>
-        Dummy Notes
-      </Typography>
-      <Grid container spacing={3}>
-        {notes.map(note => (
-          <Grid item xs={12} sm={6} md={4} key={note.id}>
-            <Card>
-              <CardContent>
-                <Typography variant="h6" component="h2">
-                  {note.title}
-                </Typography>
-                <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-                  Category: {note.category}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {JSON.parse(note.notes).content[0].content[0].text}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input type="text" className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput} />
+          <button className="btn btn-primary" type="button"
+            onClick={handleSearch}
+          >Search</button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 &&
+        <div classNa me="col-md-12 text-center mt-5">
+
+          <div className="shadow rounded wetherResultBox">
+            <img className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+
+            <h5 className="weathorCity">
+              {data?.name}
+            </h5>
+            <h6 className="weathorTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+          </div>
+        </div>
+      }
+
+    </div>
   );
 }
 
